982784fd0287b7e80de0549d9542b55a1ec07b25
Daniel Mikusa <dmikusa@gopivotal.com>
2013-09-14 18:17:28 -0400

    Added tests for hashutils

--- test/test_build_pack_utils.py
+++ test/test_build_pack_utils.py
@@ -38,4 +38,81 @@ class TestDownloaderUtils(object):
 
 
 class TestHashUtils(object):
+    def hash_file_calc(self, hsh, digest):
+        path = "./test/data/HASH"
+        assert digest == hsh.calculate_hash(path)
+
+    def hash_file_matches(self, hsh, digest):
+        path = "./test/data/HASH"
+        assert hsh.does_hash_match(digest, path)
+
+    def hash_file_not_matches(self, hsh, digest):
+        path = "./test/data/HASH"
+        assert not hsh.does_hash_match(digest, path)
+
+    # Test hashlib
+    def test_hash_util_sha1(self):
+        self.hash_file_calc(
+            build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha1'}),
+            "cf6ef7e713aeff5426e592b8e897fed7590407af")
+
+    def test_hash_util_sha256(self):
+        self.hash_file_calc(
+            build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha256'}),
+            "7cd9d6e1140dfbefecd88b7552ac6be5bf59885c604da3cf52ededb988e9fff0")
+
+    def test_hash_util_sha512(self):
+        self.hash_file_calc(
+            build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha512'}),
+            "829640ff489bbc9d12267fe5bbae69e0e65f293171a126b22bb760bc259120e53" \
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_hash_util_matches(self):
+        self.hash_file_matches(
+            build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha512'}),
+            "829640ff489bbc9d12267fe5bbae69e0e65f293171a126b22bb760bc259120e53" \
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_hash_util_not_matches(self):
+        self.hash_file_not_matches(
+            build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha512'}),
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_hash_util_no_file(self):
+        hsh = build_pack_utils.HashUtil({'cache-hash-algorithm': '512'})
+        assert '' == hsh.calculate_hash(None)
+        assert '' == hsh.calculate_hash('')
+
+    # Test External SHA
+    def test_sha_hash_util_sha1(self):
+        self.hash_file_calc(
+            build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '1'}),
+            "cf6ef7e713aeff5426e592b8e897fed7590407af")
+
+    def test_sha_hash_util_sha256(self):
+        self.hash_file_calc(
+            build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '256'}),
+            "7cd9d6e1140dfbefecd88b7552ac6be5bf59885c604da3cf52ededb988e9fff0")
+
+    def test_sha_hash_util_sha512(self):
+        self.hash_file_calc(
+            build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '512'}),
+            "829640ff489bbc9d12267fe5bbae69e0e65f293171a126b22bb760bc259120e53" \
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_sha_hash_util_matches(self):
+        self.hash_file_matches(
+            build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '512'}),
+            "829640ff489bbc9d12267fe5bbae69e0e65f293171a126b22bb760bc259120e53" \
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_sha_hash_util_not_matches(self):
+        self.hash_file_not_matches(
+            build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '512'}),
+            "fbd0e8fa973f782b27b2059b72f46c5080411f018651808a6f716eec08bc1f1")
+
+    def test_sha_hash_util_no_file(self):
+        hsh = build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '512'})
+        assert '' == hsh.calculate_hash(None)
+        assert '' == hsh.calculate_hash('')
 
