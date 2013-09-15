fa9423b7c09a861dddeb11865fe9d12ed8b0bbcf
Daniel Mikusa <dmikusa@gopivotal.com>
2013-09-14 22:09:11 -0400

    Added tests for DirectoryCacheManager

--- test/test_build_pack_utils.py
+++ test/test_build_pack_utils.py
@@ -146,3 +146,30 @@ class TestHashUtils(object):
         hsh = build_pack_utils.ShaHashUtil({'cache-hash-algorithm': '2'})
         self.hash_file_bad_algorithm(hsh, '2', 'shasum: Unrecognized algorithm')
 
+
+class TestDirectoryCacheManager(object):
+
+    def __init__(self):
+        self._hshUtil = build_pack_utils.HashUtil({'cache-hash-algorithm': 'sha256'})
+
+    def create_junk_file(self, fileName):
+        path = os.path.join(tempfile.gettempdir(), fileName)
+        with open(path, 'w') as tmp:
+            tmp.write("Hello World!")
+        return (path, self._hshUtil.calculate_hash(path))
+
+    def test_basics(self):
+        path = os.path.join(tempfile.gettempdir(), "DCM")
+        dcm = build_pack_utils.DirectoryCacheManager({
+                'file-cache-base-directory': path,
+                'use-external-hash': False,
+                'cache-hash-algorithm': 'sha256' })
+        assert not dcm.exists('asdf', None)
+        junk_file = self.create_junk_file('junk.txt')
+        key = os.path.basename(junk_file[0])
+        dcm.put(key, junk_file[0], junk_file[1])
+        assert dcm.exists(key, junk_file[1])
+        assert dcm.get(key, junk_file[1]).endswith('DCM/junk.txt')
+        dcm.delete(key)
+        assert not dcm.exists(key, junk_file[1])
+
