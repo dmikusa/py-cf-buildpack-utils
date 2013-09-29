e60482f4f4802a15770a536d2868bf9e96068588
Daniel Mikusa <dmikusa@gopivotal.com>
2013-09-29 14:03:51 -0400

    Added setup install file

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='py-cf-buildpack-utils',
+      version='1.0.0',
+      description='Python utilities for building CloudFoundry build packs',
+      author='Daniel Mikusa',
+      author_email='dan@mikusa.com',
+      url='http://www.mikusa.com/projects/py-cf-buildpack-utils',
+      license='Apache 2',
+      keywords='cloudfoundry buildpack python',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Natural Language :: English',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6'
+      ],
+      package_dir={'': 'src'},
+      py_modules=['build_pack_utils'],
+      long_description=read('README'))
+
