080a5838a63c31ff77fa4f22ffcbf67118e99bf2
Daniel Mikusa <dmikusa@pivotal.io>
2015-02-13 12:19:38 -0500

    Encode lines as utf-8, because they might contain non-ascii characters.

--- src/build_pack_utils/process.py
+++ src/build_pack_utils/process.py
@@ -216,7 +216,7 @@ class Printer(object):
 
         for arg in args:
             lines = arg.split('\n')
-            lines = [self._prefix() + l if l else l for l in lines]
+            lines = [self._prefix() + l.encode('utf-8') if l else l for l in lines]
             new_args.append('\n'.join(lines))
 
         self.output.write(*new_args, **kwargs)
